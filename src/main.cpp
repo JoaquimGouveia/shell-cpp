90f468e4511d37d03ec6fbf460ad234658481dca
Joaquim <joaquimgouveia2000@gmail.com>
2025-06-15 21:43:26 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,12 @@ int main() {
   std::string input;
   while (std::getline(std::cin, input)) {
     if (input == "exit 0") exit(0);
-    else if (input.substr(0, 4) == "echo") std::cout << input.substr(5) << std::endl;    
+    else if (input.substr(0, 4) == "echo") std::cout << input.substr(5) << std::endl;
+    else if (input.substr(0, 4) == "type") {
+      std::string param = input.substr(5);
+      if (param == "echo" || param == "type" || param == "exit") std::cout << param << " is a shell builtin" << std::endl;
+      else std::cout << param << ": not found" << std::endl;
+    }
     else {
       std::cout << input << ": command not found" << std::endl;
     }
