455d758747d0a41a328fe33d50189ce72ebb434f
Joaquim <joaquimgouveia2000@gmail.com>
2025-06-15 21:20:48 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ int main() {
   std::cout << "$ ";
 
   std::string input;
-  std::getline(std::cin, input);
-
-  std::cout << input << ": command not found" << std::endl;
+  while (std::getline(std::cin, input)) {
+    std::cout << input << ": command not found" << std::endl;
+    std::cout << "$ ";
+  }
 }
